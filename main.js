049be93a7c2e1cdd2f296d43177ca5e4b0614726
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-10 16:19:08 +0200

    Refactor main.js to include Snack 2 functionality for football teams and randomize points and fouls

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Snack 1
-/* Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore. */
+/* // Snack 1
+/* Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore. 
 
 const bikes = [
     {
@@ -45,4 +45,48 @@ for (let i = 0; i < bikes.length; i++) {
     }
 }
 
-console.log(`La bici con il peso minore è: ${minWeight.name} peso: ${minWeight.weight}`);
+console.log(`La bici con il peso minore è: ${minWeight.name} peso: ${minWeight.weight}`); */
+
+/* Snack 2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+const teams = [
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Inter",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Juventus",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Real Madrid",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Napoli",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }
+]
+const nomeFalli = [];
+
+for (let i = 0; i < teams.length; i++) {
+    thisTeam = teams[i];
+    thisTeam.puntiFatti = Math.ceil(Math.random() * 1000);
+    thisTeam.falliSubiti = Math.ceil(Math.random() * 1000);
+    nomeFalli[i] = { nome: thisTeam.nome, falliSubiti: thisTeam.falliSubiti };
+}
+
+console.log(teams);
+console.log(nomeFalli);
